f87215e2257f6b0ca544a0653677b58ec1e4f0e2
kousik <kousikgvs@gmail.com>
2023-05-25 01:44:12 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import Body from "./Body/Body";
 import Header from "./Header/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Body />} />
-      </Routes>
-    </Router>
+      <Body />
+    </>
   );
 };
 
